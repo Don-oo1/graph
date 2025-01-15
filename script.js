3e1c54a2115c96b61e54327ba1811e7e4821706c
Don-001 <nirmalodedara007@gmail.com>
2025-01-15 12:26:48 +0530

    rander the data on web page on grap using chart.js

--- script.js
+++ script.js
@@ -4,13 +4,41 @@
 
 getData()
 .catch(err=>{
-    console.log(err)
+    // console.log(err)
 })
 
 async function getData() {
 
     const response = await fetch("temperature.txt")
     const text = await response.text()
-    console.log(text)
-    
+    // console.log(text)
+    const years = [], noSmoothing = [], lowess = []
+
+    const lines  = text.split("\n")
+    for(const line of lines){
+        const column = line.split(`     `)
+        years.push(column[0])
+        noSmoothing.push(column[1])
+        lowess.push(column[2])
+    }
+
+
+     console.log(years)
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: years,
+        datasets: [{
+          label: '# of No_Smoothing',
+          data: noSmoothing,
+           borderWidth: 1
+        }]
+      },
+     
+    });
+
+
+
 }
